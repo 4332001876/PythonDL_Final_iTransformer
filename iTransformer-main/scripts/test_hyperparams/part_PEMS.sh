7948e36cd55afcd6445952313d70f607114dc60d
4332001876 <mzfslhm@163.com>
2024-01-31 19:42:36 +0800

    huge table; paper understanding

---
+++ iTransformer-main/scripts/test_hyperparams/part_PEMS.sh
@@ -0,0 +1,67 @@
+export CUDA_VISIBLE_DEVICES=1
+
+model_name=iTransformer
+
+python -u run.py \
+  --is_training 1 \
+  --root_path ./dataset/PEMS/ \
+  --data_path PEMS03.npz \
+  --model_id PEMS03_96_96 \
+  --model $model_name \
+  --data PEMS \
+  --features M \
+  --seq_len 96 \
+  --pred_len 96 \
+  --e_layers 4 \
+  --enc_in 358 \
+  --dec_in 358 \
+  --c_out 358 \
+  --des 'Exp' \
+  --d_model 512 \
+  --d_ff 512 \
+  --learning_rate 0.0001 \
+  --itr 1
+
+python -u run.py \
+  --is_training 1 \
+  --root_path ./dataset/PEMS/ \
+  --data_path PEMS07.npz \
+  --model_id PEMS07_96_48 \
+  --model $model_name \
+  --data PEMS \
+  --features M \
+  --seq_len 96 \
+  --pred_len 48 \
+  --e_layers 4 \
+  --enc_in 883 \
+  --dec_in 883 \
+  --c_out 883 \
+  --des 'Exp' \
+  --d_model 512 \
+  --d_ff 512 \
+  --batch_size 16\
+  --learning_rate 0.0001 \
+  --itr 1 \
+  --use_norm 0
+
+python -u run.py \
+  --is_training 1 \
+  --root_path ./dataset/PEMS/ \
+  --data_path PEMS07.npz \
+  --model_id PEMS07_96_96 \
+  --model $model_name \
+  --data PEMS \
+  --features M \
+  --seq_len 96 \
+  --pred_len 96 \
+  --e_layers 4 \
+  --enc_in 883 \
+  --dec_in 883 \
+  --c_out 883 \
+  --des 'Exp' \
+  --d_model 512 \
+  --d_ff 512 \
+  --batch_size 16\
+  --learning_rate 0.0001 \
+  --itr 1 \
+  --use_norm 0
